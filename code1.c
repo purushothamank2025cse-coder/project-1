ba7ae5eee3fac34b7152c010670baa13db94cc09
purushothamank2025cse-coder <purushothaman.k2025cse@sece.ac.in>
2025-11-24 11:05:44 +0530

    alphabets

---
+++ code1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   for(int i=97;i<=122;i++)
+   {
+       printf("%c\t",i);
+      printf("%c\t",i);
+   }
+   for(int i=65;i<=90;i++)
+   {
+    printf("%c\t",i);
+   }
+}
